75f36dec09244959519b8d09af97f977c9b5c7f5
Artiom Cozlovici <artiom.cozlovici@deeplace.md>
2018-07-09 15:52:24 +0300

    operation_history added

--- pygolos/api.py
+++ pygolos/api.py
@@ -3,21 +3,27 @@ from json import dumps
 from json import loads
 from pygolos.witness_api import WitnessApi
 from pygolos.account_history import AccountHistory
+from pygolos.operation_history import OperationHistory
 
 
 class Api:
     def __init__(self, url="wss://ws.golos.io"):
         self.__ws = create_connection(url)
-        self._witness = WitnessApi(self)
-        self._account_history = AccountHistory(self)
+        self.__witness = WitnessApi(self)
+        self.__account_history = AccountHistory(self)
+        self.__operation_history = OperationHistory(self)
 
     @property
     def witness(self):
-        return self._witness
+        return self.__witness
 
     @property
     def account_history(self):
-        return self._account_history
+        return self.__account_history
+
+    @property
+    def operation_history(self):
+        return self.__operation_history
 
     def __call(self, api, method, params):
         self.__ws.send(dumps({"method": "call", "jsonrpc": "2.0",
