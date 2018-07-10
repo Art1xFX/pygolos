0da5775a8545b9131533b0e014a7880e375c5e41
valentine <serebreanu.v@gmail.com>
2018-07-10 08:22:22 -0400

    follow_api

--- pygolos/api.py
+++ pygolos/api.py
@@ -1,48 +1,35 @@
 from websocket import create_connection
 from json import dumps
 from json import loads
-from pygolos.witness_api import WitnessApi
-from pygolos.account_history import AccountHistory
-from pygolos.operation_history import OperationHistory
-from pygolos.tags import Tags
-from pygolos.social_network import SocialNetwork
+
 from pygolos.database_api import DatabaseApi
+from pygolos.follow_api import FollowApi
+from pygolos.witness_api import WitnessApi
 
 
 class Api:
     def __init__(self, url="wss://ws.golos.io"):
         self.__ws = create_connection(url)
-        self.__witness = WitnessApi(self)
-        self.__account_history = AccountHistory(self)
-        self.__operation_history = OperationHistory(self)
-        self.__tags = Tags(self)
-        self.__social_network = SocialNetwork(self)
+        self._witness = WitnessApi(self)
         self._database=DatabaseApi(self)
+        self._follow=FollowApi(self)
 
     @property
     def witness(self):
-        return self.__witness
-
-    @property
-    def account_history(self):
-        return self.__account_history
-
-    @property
-    def operation_history(self):
-        return self.__operation_history
-
-    @property
-    def tags(self):
-        return self.__tags
-
-    @property
-    def social_network(self):
-        return self.__social_network
+        return self._witness
 
     @property
     def database(self):
         return self._database
-      
+
+    @property
+    def follow(self):
+        return self._follow
+
+    @property
+    def account_history(self):
+        pass
+
     def __call(self, api, method, params):
         self.__ws.send(dumps({"method": "call", "jsonrpc": "2.0",
                               "params": [api, method, params]}))
